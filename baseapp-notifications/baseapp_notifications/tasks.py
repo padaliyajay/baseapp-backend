32312f35a13dd76e202b851e09dbd6c77cbc7021
vitorguima <vitorguimaraes@Vitors-MacBook-Pro.local>
2024-01-08 14:01:11 -0300

    baseapp-backend-push-notifications

--- baseapp-notifications/baseapp_notifications/tasks.py
+++ baseapp-notifications/baseapp_notifications/tasks.py
@@ -1,11 +1,23 @@
 from celery import shared_task
 
-
 @shared_task
 def send_push_notification(user_id, data):
-    # TO BE DONE
-    # get user's device tokens and send to each of them
-    # this function should be easy to override per project since
-    # some projects might use different push notification services
-    # like, expo, firebase, APN and so on
+    from push_notifications.models import APNSDevice, GCMDevice, WNSDevice, WebPushDevice
+
+    # send messages to all Apple devices
+    apple_devices = APNSDevice.objects.filter(user__id=user_id)
+    apple_devices.send_message(data)
+
+    # send messages to all Android devices
+    android_devices = GCMDevice.objects.filter(user__id=user_id)
+    android_devices.send_message(data)
+
+    # send messages to all Windows devices
+    windows_devices = WNSDevice.objects.filter(user__id=user_id)
+    windows_devices.send_message(data)
+
+    # send messages to all Web devices
+    web_devices = WebPushDevice.objects.filter(user__id=user_id)
+    web_devices.send_message(data)
+
     pass
